d10f7d71290a0532a86bd9dbc07b1488540b0028
Lee Song <leesong1995@hotmail.com>
2016-12-15 01:52:39 +0800

    Add project files.

---
+++ WarkeyNETIII/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WarkeyNETIII.Pages;
+using WarkeyNETIII.ViewModels;
+
+namespace WarkeyNETIII
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel vm = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+
+            NavFrame.Navigate(new WarkeyPage());
+        }
+
+        private void startAnimationByName(string name)
+        {
+            Storyboard sb = this.FindResource(name) as Storyboard;
+            sb.Begin();
+        }
+
+        private void HamButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (HamMenu.Width == 50)
+                startAnimationByName("SideOpen");
+            else if (HamMenu.Width == 200)
+                startAnimationByName("SideClose");
+        }
+
+        private void WarkeyPageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavFrame.Navigate(new WarkeyPage());
+        }
+    }
+}
---
+++ WarkeyNETIII/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarkeyNETIII.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        private string title;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Title = "Warkey.NET III";
+        }
+
+    }
+}
