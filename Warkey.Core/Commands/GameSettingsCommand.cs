a5a9c19a190658f3649eeba174b0d681bfce445a
noobhacker <leesong1995@gmail.com>
2019-07-11 11:23:26 +0800

    refactored gamevideosettingsservice into cqrs

---
+++ Warkey.Core/Commands/GameSettingsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warkey.Infrastructure;
+
+namespace Warkey.Core.Commands
+{
+    public class GameSettingsCommand
+    {
+        private readonly GameSettings _gameSettings;
+
+        public GameSettingsCommand()
+        {
+            _gameSettings = new GameSettings();
+        }
+
+        public void WriteResolution(int width, int height)
+        {
+            _gameSettings.WriteVideoSettings("reswidth", width);
+            _gameSettings.WriteVideoSettings("resheight", height);
+        }
+
+        public void WriteLockFb()
+        {
+            _gameSettings.WriteVideoSettings("lockfb", 0);
+        }
+    }
+}
